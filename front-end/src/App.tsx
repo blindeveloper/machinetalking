ef2539aa68c225ad9b20e460414092ee567a15c6
blindeveloper <m.shavkunov@gmail.com>
2025-06-10 15:17:50 +0200

    adding vite + mantine

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,29 +1,14 @@
-import './App.css';
-import SubmitForm from './components/SubmitForm';
+import './App.css'
+import { Input } from '@mantine/core'
 
-const App = () => {
+function App() {
   return (
-    <div className="content">
-        <header>
-            <h1>ML AI Projects 👾</h1>
-        </header>
-        <main>
-            <section class="project">
-                <h2>Project 1: Image Classification</h2>
-                <p>Description: A project to classify images using a convolutional neural networks.</p>
-                <p>Results: Achieved 95% accuracy on the test dataset.</p>
-            </section>
-            <section class="project">
-                <h2>Project 2: Sentiment Analysis</h2>
-                <p>Description: A project to analyze the sentiment of text data using natural language processing.</p>
-                <p>Results: Achieved 90% accuracy on the sentiment analysis task.</p>
-            </section>
-            <section>
-                <SubmitForm />
-            </section>
-        </main>
+    <>
+    <div>
+        <Input placeholder="Input component" />
     </div>
-  );
-};
+    </>
+  )
+}
 
-export default App;
+export default App
