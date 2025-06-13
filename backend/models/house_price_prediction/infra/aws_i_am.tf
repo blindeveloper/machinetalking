706a954cdfb9ff99956662453d5e9f441475a998
blindeveloper <m.shavkunov@gmail.com>
2025-06-13 14:38:34 +0200

    tf changes

--- backend/models/house_price_prediction/infra/aws_i_am.tf
+++ backend/models/house_price_prediction/infra/aws_i_am.tf
@@ -5,12 +5,12 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }]
   })
 }
 
@@ -47,4 +47,9 @@ resource "aws_iam_policy" "lambda_s3_policy" {
       Resource = "${aws_s3_bucket.model_bucket.arn}/*"
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
